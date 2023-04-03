52d07eb37ef4a69c6971c77bde9a6999f269c570
Ali Hamxa <ahamxa007@gmail.com>
2023-04-03 15:09:35 +0400

    Update App.jsx
    Changed the title

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [tableData, setTableData] = useState(Data);
   return (
     <div className="px-4 md:px-8 lg:container lg:mx-auto">
-      <h1 className="text-3xl font-bold underline mt-32">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline mt-32">HCT Form</h1>
       <div className="lg:grid lg:grid-cols-2 lg:gap-48">
         <Form tableData={tableData} setTableData={setTableData} />
         <Table tableData={tableData} />
